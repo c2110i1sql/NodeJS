54eab42a549f0353441441f93ba107cee32a27f8
Lớp C2110I1 <c2110i1sql@gmail.com>
2023-01-05 14:29:22 +0700

    validate form voi Joi module

--- routes/category.js
+++ routes/category.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const conn = require('../connect');
 
 module.exports = function(server) {
@@ -5,20 +6,41 @@ module.exports = function(server) {
         conn.query("SELECT * FROM categories", function(err, data) {
             if (err) return new Error("Loi truy va " + err.sqlMessage)
             res.render('admin/category/index', {
-                cats: data
+                cats: data,
+                errors: null
             })
         })
     });
-    
+
     server.get('/admin/category/create', function(req, res) {
-        res.render('admin/category/create');
+        res.render('admin/category/create', {
+            errors: null
+        })
     });
 
-    server.post('/admin/category/store', function(req, res) {
-        conn.query("INSERT INTO categories SET ?", req.body, function(err, data) {
-            if (err) return new Error("Loi truy va " + err.sqlMessage)
-            res.redirect('/admin/category');
-        })
+    server.post('/admin/category/create', function(req, res) {
+        // validate 
+        schema = Joi.object().keys({ 
+            name: Joi.string().required().min(3).messages({
+                "string.empty": "Tên danh mục không để trống",
+                "string.min" : "Tên danh mục ít nhất là 3 ký tự"
+            }),
+            status: Joi.number().required().messages({"number.empty": "Trang thai khong duoc de trong"})
+        }); 
+
+        const {error} = schema.validate(req.body);
+
+        if (error) {
+            res.render('admin/category/create', {
+                errors: error.details
+            });
+        } else {
+            conn.query("INSERT INTO categories SET ?", req.body, function(err, data) {
+                if (err) return new Error("Loi truy va " + err.sqlMessage)
+                res.redirect('/admin/category');
+            })
+        }
+       
     });
 
     server.get('/admin/category/edit/:id', function(req, res) {

4fb9da7f427f8bc91a54353319360538edb351b6
Lớp C2110I1 <c2110i1sql@gmail.com>
2023-01-10 15:06:35 +0700

    Dang nhap admin Exxpress-session

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,6 @@
 // file: routes/admin.js
 module.exports = function(server) {
     server.get('/admin', function(req, res) {
-        console.log(req.session.login);
         res.render('admin/index')
-    });
-
-    server.get('/admin/login', function(req, res) {
-        res.render('admin/login')
-    });
-
-    server.post('/admin/login', function(req, res) {
-        req.session.login = req.body;
-        res.redirect('/admin');
     });
 }
--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const ejs = require('ejs');
 
-
 const server = express();
+// global.login = 'sdsdsds';  // global trong mã nodejs
+// server.locals.email = 'adndc@gmail.cmol'; // global toàn bộ các file view
 
 server.use(session({
     secret: 'bkap-session',
@@ -12,14 +13,33 @@ server.use(session({
     saveUninitialized: false
 }));
 
+
+
+// truyền biến global
+// global trong mã nodejs
+// global toàn bộ các file view
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(bodyParser.json()); //. api
 server.use(express.static('public'));
 server.set('view engine', 'html');
 server.engine('html',ejs.renderFile);
 
-// chia router
+// khong can kiemr tra login van vao duoc
+require('./routes/login')(server);
 require('./routes/home')(server);
+
+// middleware => kieemr tra login truoc khi vao cacs  router
+server.use(function(req, res, next) {
+    if (!req.session.login) {
+        res.redirect('/admin/login');
+    } else {
+        server.locals.name = req.session.login.name;
+    }
+    
+    next();
+})
+
+// chia router
 require('./routes/admin')(server);
 require('./routes/category')(server);
 require('./routes/product')(server);
